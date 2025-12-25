0219aa62e52d9d9c43c3b636a121b69a23acea5a
Dhanunjai-Miriyala <63292968+Dhanunjai-Miriyala@users.noreply.github.com>
2025-12-26 03:42:42 +0530

    Delete resume-portfolio-builder directory

--- resume-portfolio-builder/src/templates/index.ts
+++
@@ -1,9 +0,0 @@
-import DefaultTemplate from "./default/Template";
-import MinimalTemplate from "./minimal/Template";
-
-export const templates = {
-  default: DefaultTemplate,
-  minimal: MinimalTemplate,
-};
-
-export type TemplateKey = keyof typeof templates;
